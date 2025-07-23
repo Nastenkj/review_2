ccb748f083ae7e675b3e2da3653a22e28d5a1f5a
Русинова Анастасия <112797645+Nastenkj@users.noreply.github.com>
2025-07-24 00:17:15 +0300

    Add files via upload

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,16 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'jsdom',
   transform: {
     ...tsJestTransformCfg,
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(svg|png|jpg|jpeg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^@ui$': '<rootDir>/src/components/ui',
+    '^@ui/(.*)$': '<rootDir>/src/components/ui/$1',
+    '^@components$': '<rootDir>/src/components',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
   },
 };
